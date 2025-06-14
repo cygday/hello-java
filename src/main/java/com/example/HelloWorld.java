cfe8be9338137a5d65044d30ecc934a0f1302ee9
cygday <amethystpun98@gmail.com>
2025-06-14 12:49:30 +0545

    updated src directory

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.example;
+
 public class HelloWorld {
           public static void main(String[] args) {
             System.out.println("do more projects boss, don't get upset for future!");
